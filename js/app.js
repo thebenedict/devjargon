27827cd19028dcf733e878da3835969d7a033025
thebenedict <thebenedict@gmail.com>
2014-09-10 16:58:02 -0400

    styles, forms and content!

--- js/app.js
+++ js/app.js
@@ -2,29 +2,39 @@ var app = app || {};
 
 var ROOT = 'http://api.devjargon.org'
 
-$(':button').click(function(){
-    var formData = new FormData($('form')[0]);
-    $.ajax({
-        url: 'http://api.devjargon.org/documents/',
-        data: formData,
-        type: 'POST',
-        xhr: function() {
-            return $.ajaxSettings.xhr();
-        },
-        success: function(data) {
-        	destination = "#/documents/" + data['id']
-        	window.location.href = destination;
-        },
-        error: function(e) {
-        	//TODO
-        	console.log('error')
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-});
-
 $(function() {
 	new app.AppView();
+
+    $('#submit-file').click(function(){
+        var formData = new FormData($('form')[0]);
+        $.ajax({
+            url: 'http://api.devjargon.org/documents/',
+            data: formData,
+            type: 'POST',
+            xhr: function() {
+                return $.ajaxSettings.xhr();
+            },
+            beforeSend: function() {
+                $('#submit-file').prop('disabled', true);
+                $('#submit-file').prop('value', 'Finding jargon...')
+            },
+            success: function(data) {
+                destination = "#/documents/" + data['id']
+                window.location.href = destination;
+            },
+            error: function() {
+                $('#submit-file').prop('value', 'An error occurred')
+                setTimeout(function() { 
+                    window.location.href = "/";
+                }, 2000);
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+    });
+
+    $(document).on('click', '#start-over', function() {
+        window.location.href = "/";
+    });    
 });
