1c33e097d8a60c82126f51abe6cfa114ca6f80ac
thebenedict <thebenedict@gmail.com>
2014-09-09 23:46:13 -0400

    turn off push state

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(':button').click(function(){
             return $.ajaxSettings.xhr();
         },
         success: function(data) {
-        	destination = "/documents/" + data['id']
+        	destination = "#/documents/" + data['id']
         	window.location.href = destination;
         },
         error: function(e) {
